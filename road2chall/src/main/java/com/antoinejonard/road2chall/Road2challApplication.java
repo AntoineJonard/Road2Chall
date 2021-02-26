bad614c2b7e744662727e6c96dbb73f620263803
AntoineJonard <antoine.jonard@orange.fr>
2021-02-26 14:29:06 +0100

    init

---
+++ road2chall/src/main/java/com/antoinejonard/road2chall/Road2challApplication.java
@@ -0,0 +1,13 @@
+package com.antoinejonard.road2chall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Road2challApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Road2challApplication.class, args);
+	}
+
+}
---
+++ road2chall/src/test/java/com/antoinejonard/road2chall/Road2challApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antoinejonard.road2chall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Road2challApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
