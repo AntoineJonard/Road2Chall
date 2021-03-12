acc0d2d19da2a75407724b65fe881f0c5abc62b1
AntoineJonard <antoine.jonard@orange.fr>
2021-03-12 09:26:42 +0100

    liste des users

--- road2chall/src/main/java/com/antoinejonard/road2chall/manager/UserManager.java
+++ road2chall/src/main/java/com/antoinejonard/road2chall/manager/UserManager.java
@@ -1,15 +1,11 @@
 package com.antoinejonard.road2chall.manager;
-
-import com.antoinejonard.road2chall.model.Team;
 import com.antoinejonard.road2chall.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
-
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Path("user")
 public class UserManager {
@@ -29,15 +25,16 @@ public class UserManager {
     @PUT
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/createUser")
-    public Response createUser(@QueryParam("name") String name, @QueryParam("pwd") String pwd){
+    public User createUser(@QueryParam("name") String name, @QueryParam("pwd") String pwd){
         List<User> usersWithSameName = userRepository.findByName(name);
         User user;
         if(usersWithSameName.isEmpty())
             user = new User(name, pwd, new ArrayList<>());
         else
-            return Response.status(Response.Status.NOT_ACCEPTABLE).build();
+            return null;
+            //return Response.status(Response.Status.NOT_ACCEPTABLE).build();
         userRepository.save(user);
-        return Response.ok(user).build();
+        return user;
     }
 
     @POST
---
+++ road2chall/src/main/resources/public/hello.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+
+    $.get("http://localhost:8080/road2Chall/user/allUsers",function(resp){
+    	$.each(resp, function(index, item) {
+    		console.log(item);
+    		$('#list').append('<li id="personne-'+item.id+'" class="list-group-item">'+item.name+' '+item.pwd+' <button class="btn btn-danger btn-xs" data-title="Delete" data-toggle="modal" data-target="#delete" >X</button></li>');
+    	});
+    });
+
+    $('#list').delegate("li button", "click", function() {
+    	console.log(this);
+    	var elemid = $(this).parent().attr('id');
+
+    	$.ajax({
+		    type: "DELETE",
+		    url: "http://localhost:8080/api/personnes/" + elemid.replace('personne-',''),
+		    dataType: "json",
+		    success: function(data){
+		    	$("#"+elemid).remove();
+		    }
+		});
+    });
+
+    $('#addbtn').click(function(){
+		var name = $('#name-input').val();
+		var pwd = $('#pwd-input').val();
+
+		$.ajax({
+		    type: "PUT",
+		    url: "http://localhost:8080/road2Chall/user/createUser",
+		    data: JSON.stringify({ "name": name, "pwd" : pwd }),
+		    contentType: "application/json; charset=utf-8",
+		    dataType: "json",
+		    success: function(data){
+		    	console.log(data);
+		    	$('#list').append('<li id="personne-'+data.id+'" class="list-group-item">'+data.name+' '+data.pwd+' <button class="btn btn-danger btn-xs" data-title="Delete" data-toggle="modal" data-target="#delete" >X</button></li>');
+		    }
+		});
+
+
+		$('#name-input').val('');
+		$('#pwd-input').val('');
+		return false;
+	});
+});
